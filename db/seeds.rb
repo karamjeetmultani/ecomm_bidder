3049daf34ec313a856352033c0beb1e36e3b7e03
Ubuntu <ubuntu@ip-172-31-86-87.ec2.internal>
2022-12-06 04:20:28 +0000

    new changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,7 @@
   # user = User.create!({:email => "#{first_name}_#{last_name}_#{usr}@sample.com", :password => "P@ssw0rd987654321", :password_confirmation => "P@ssw0rd987654321", :first_name => first_name, :last_name => last_name, :phone_number => 1111111111})
   user = User.create!({:email => "example#{usr}@sample.com", :password => "password", :password_confirmation => "password", :first_name => first_name, :last_name => last_name, :phone_number => "1111111111#{usr}".to_i})
   puts user
+  user.create_role(:role_type => 'admin')
   (0..4).each do |val|
     ["open", "closed"].each do |p|
       Product.create(:name => Faker::Commerce.product_name, :barcode => "1111#{usr}#{val}#{val}".to_i, :price => Faker::Commerce.price, :bid_status => p, :bid_enable => true, :user_id => user.id)
@@ -18,9 +19,18 @@
   end
 end
 
-usr1 = User.first
-usr1.create_role(:role_type => 'admin')
-usr2 = User.last
-usr2.create_role(:role_type => 'normal')
+(5..14).each do |usr|
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  user = User.create!({:email => "example#{usr}@sample.com", :password => "password", :password_confirmation => "password", :first_name => first_name, :last_name => last_name, :phone_number => "1111111111#{usr}".to_i})
+  puts user
+  user.create_role(:role_type => 'normal')
+
+end
+
+#usr1 = User.first
+#usr1.create_role(:role_type => 'admin')
+#usr2 = User.last
+#usr2.create_role(:role_type => 'normal')
 AddressType.create(:address_type => "billing")
 AddressType.create(:address_type => "shipping")
