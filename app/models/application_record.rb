645db1ff3f436e62a9c3553f8e52e949761a6217
karamjeetmultani <karamjeet.multani@gmail.com>
2022-12-07 00:51:01 -0500

    updated time for the messages

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,6 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
   def create_new_message(user_id, message)
-    Notification.create(:user_id => user_id, :message => message)
+    User.where(:notification_enable => true).pluck(:id).each do |id|
+      Notification.create(:user_id => id, :message => message)
+    end
   end
 end
