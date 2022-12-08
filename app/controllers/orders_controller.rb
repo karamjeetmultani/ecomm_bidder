9fcc3b75913c368d38eb003249b7073108d7ad38
Ubuntu <ubuntu@ip-172-31-86-87.ec2.internal>
2022-12-08 05:25:18 +0000

    new changes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,7 @@ class OrdersController < ApplicationController
 
   # GET /orders or /orders.json
   def index
-    @orders = Order.all
+    @orders = current_user.orders
   end
 
   # GET /orders/1 or /orders/1.json
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   has_one :role
   has_many :reviews
   has_many :notifications
+  has_many :orders
   def full_name
     return "#{self.first_name} #{self.last_name}"
   end
